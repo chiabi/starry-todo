31b63b8112399ec1222c013ac202cef7f67d1b46
chiabi <chiabi88@gmail.com>
2018-05-27 21:39:58 +0900

    bulma.css 테스트

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+// function test(func) {
+//   const length = 100000;
+//   const t1 = performance.now();
+//   for(let i = 0; i < length; i++) {
+//     func(i);
+//   }
+//   const t2 = preformance.now();
+//   console.log(`test start: ${t2 - t1}ms`);
+// }
+
+const templates = {
+  login: document.querySelector('.login').contents;
+}
