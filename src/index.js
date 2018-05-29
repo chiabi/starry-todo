ed5a8ced60ace039bd728a591e96b201d2e60d7a
chiabi <chiabi88@gmail.com>
2018-05-30 02:35:54 +0900

    작업 입력 모달 구현, 라벨 데이터 다루는 부분 구현 필요함

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import moment from 'moment'
 const starryAPI = axios.create({
   baseURL: process.env.API_URL
 })
+
+const body = document.querySelector('body')
 const root = document.querySelector('.root')
+
 const templates = {
-  login: document.querySelector('#login').content,
-  register: document.querySelector('#register').content,
-  notification: document.querySelector('#notification').content,
-  loading: document.querySelector('#loading').content,
-  index: document.querySelector('#index').content,
+  login:          document.querySelector('#login').content,
+  register:       document.querySelector('#register').content,
+  notification:   document.querySelector('#notification').content,
+  loading:        document.querySelector('#loading').content,
+  index:          document.querySelector('#index').content,
   projectContent: document.querySelector('#project-content').content,
-  projectItem: document.querySelector('#project-item').content,
-  taskItem: document.querySelector('#task-item').content,
-  taskModal: document.querySelector('#task-write-modal').content,
+  projectItem:    document.querySelector('#project-item').content,
+  taskItem:       document.querySelector('#task-item').content,
+  taskLabel:       document.querySelector('#task-label').content,
+  taskWriteModal: document.querySelector('#task-write-modal').content,
+}
+
+function deepCopyFragment(template) {
+  return document.importNode(template, true)
 }
 
 function render(fragment, parent = root, clear = true) {
@@ -34,15 +42,17 @@ function logout() {
   delete starryAPI.defaults.headers['Authorization']
 }
 
-// notification
+// Notification Component
 function notification(message, parentNode, stateClass) {
   const fragment = document.importNode(templates.notification, true);
   const notification = fragment.querySelector('.notification');
+  
   notification.textContent = message
   notification.classList.add(stateClass)
   render(fragment, parentNode)
 }
 
+// Sign Submit Button Event
 function signSubmit(path, {form, btnSubmit}, failMessage) {
   form.addEventListener('submit', async e => {
     e.preventDefault();
@@ -52,6 +62,7 @@ function signSubmit(path, {form, btnSubmit}, failMessage) {
       password: e.target.elements.password.value,
     }
     const notificationEl = form.querySelector('.sign-form__notification');
+    
     try {
       const res = await starryAPI.post(path, payload)
       login(res.data.token)
@@ -69,6 +80,7 @@ function signSubmit(path, {form, btnSubmit}, failMessage) {
   })
 }
 
+// Sign Link Button Event
 function signLink(btnLink, func) {
   btnLink.addEventListener('click', e => {
     e.preventDefault()
@@ -76,47 +88,49 @@ function signLink(btnLink, func) {
   })
 }
 
-// login page
+// Login Page
 async function loginPage() {
-  const fragment = document.importNode(templates.login, true)
+  const fragment = deepCopyFragment(templates.login)
   const signForm = {
     form: fragment.querySelector('.login-form'),
     btnSubmit: fragment.querySelector('.login-form__btn-submit'),
   }
   const btnLink = fragment.querySelector('.login-form__btn-link')
+
   const message = 'The username or password is not correct'
   signSubmit('users/login', signForm, message)
   signLink(btnLink, registerPage)
   render(fragment)
 }
 
-// register page
+// Register Page
 async function registerPage() {
-  const fragment = document.importNode(templates.register, true)
+  const fragment = deepCopyFragment(templates.register)
   const signForm = {
     form: fragment.querySelector('.register-form'),
     btnSubmit: fragment.querySelector('.register-form__btn-submit'),
   }
   const btnLink = fragment.querySelector('.register-form__btn-link')
+
   const message = 'Username already exists'
   signSubmit('users/register', signForm, message)
   signLink(btnLink, loginPage)
   render(fragment)
 }
 
-// loading
-async function withlLoading(promise) {
-  const fragment = document.importNode(templates.loading, true)
+// Loading Component
+async function withlLoading(promise, parentEl = body) {
+  const fragment = deepCopyFragment(templates.loading)
   const el = fragment.querySelector('.loading')
-  render(fragment, document.body, false)
+  render(fragment, parentEl, false)
   const value = await promise
   el.remove()
   return value
 }
 
-// indexPage
+// Index Page
 async function indexPage() {
-  const fragment = document.importNode(templates.index, true)
+  const fragment = deepCopyFragment(templates.index)
   const btnLogout = fragment.querySelector('.header__btn-logout')
   const contentEl = fragment.querySelector('.contents');
 
@@ -128,7 +142,7 @@ async function indexPage() {
   render(fragment)
 }
 
-
+// Project Notification Component
 function projectNotification(message = `Network error please try again later`) {
   const notificationEl = document.querySelector('.project-notification')
   notification(message, notificationEl, 'is-warning')
@@ -137,6 +151,7 @@ function projectNotification(message = `Network error please try again later`) {
   }, 3000)
 }
 
+// Form Element Blur
 function inputCompleteBlur(el) {
   el.addEventListener('keydown', e => {
     if(e.keyCode === 13) {
@@ -146,9 +161,9 @@ function inputCompleteBlur(el) {
   })
 }
 
-// project content
+// Project Content Component
 async function projectContent(parentEl) {
-  const fragment = document.importNode(templates.projectContent, true)
+  const fragment = deepCopyFragment(templates.projectContent)
   const listEl = fragment.querySelector('.project-list')
   const formEl = fragment.querySelector('.project-write-form')
   const btnAddEl = formEl.querySelector('.projects__btn-add-project')
@@ -160,7 +175,7 @@ async function projectContent(parentEl) {
     inputEl.focus()
   })
 
-  // POST - project item
+  // [POST] - project item
   inputCompleteBlur(inputEl)
   inputEl.addEventListener('blur', async e => {
     e.preventDefault()
@@ -189,9 +204,9 @@ async function projectContent(parentEl) {
   render(fragment, parentEl, false)
 }
 
-// projects item
+// Project Item Component
 async function projectItem(parentEl, {title, id}) {
-  const fragment = document.importNode(templates.projectItem, true)
+  const fragment = deepCopyFragment(templates.projectItem)
   const listEl = fragment.querySelector('.task-list')
   const btnDeleteEl = fragment.querySelector('.project-item__btn-delete')
   const btnAddEl = fragment.querySelector('.project-item__btn-add-task')
@@ -203,13 +218,13 @@ async function projectItem(parentEl, {title, id}) {
   titleShadowEl.textContent = title
   titleInputEl.value = title
 
-  // GET - render task item
+  // [GET] - render task item
   const res = await starryAPI.get(`/projects/${id}/tasks`)
   for (const data of res.data) {
     taskItem(listEl, data)
   }
 
-  // DELETE - project item
+  // [DELETE] - project item
   btnDeleteEl.addEventListener('click', async e => {
     const removeEl = btnDeleteEl.closest('.project-item')
     removeEl.classList.add('project-item--delete')
@@ -222,7 +237,7 @@ async function projectItem(parentEl, {title, id}) {
     }
   })
 
-  // PATCH - project item
+  // [PATCH] - project item
   titleInputEl.addEventListener('keydown', e => {
     titleShadowEl.textContent = e.target.value
   })
@@ -239,21 +254,22 @@ async function projectItem(parentEl, {title, id}) {
     }
   })
 
-  // POST - task item
   btnAddEl.addEventListener('click', e => {
-
+    taskWriteModal(listEl, id)
   })
   
   render(fragment, parentEl, false)
 }
 
-// task item
-async function taskItem(parentEl, {title, startDate, dueDate, labelId, complete}) {
-  const fragment = document.importNode(templates.taskItem, true)
+// Task Item Component
+async function taskItem(parentEl, {title, startDate, dueDate, labelId, complete, id}) {
+  const fragment = deepCopyFragment(templates.taskItem)
   const titleEl = fragment.querySelector('.task-item__title')
   const checkEl = fragment.querySelector('.task-item__complete-check')
   const dateEl = fragment.querySelector('.task-item__date')
   const labelEl = fragment.querySelector('.task-item__label')
+  const btnDeleteEl = fragment.querySelector('.task-item__btn-delete')
+  const checkBoxEl = fragment.querySelector('.task-item__complete-check')
 
   if(complete) {
     checkEl.setAttribute('checked', '')
@@ -262,34 +278,91 @@ async function taskItem(parentEl, {title, startDate, dueDate, labelId, complete}
   dateEl.querySelector('.start').textContent = startDate
   dateEl.querySelector('.due').textContent = dueDate
 
-  // GET - label 
-  const res = await starryAPI.get(`/labels/${labelId}`)
-  switch (res.data.color) {
-    case 'yellow':
-      labelEl.classList.add('is-warning')
-      break
-    case 'green':
-      labelEl.classList.add('is-success')
-      break
-    case 'blue':
-      lableEl.classList.add('is-info')
-      break
-    case 'purple': 
-      labelEl.classList.add('is-primary')
-      break
-    case 'no':
-      labelEl.calssList.add('no-color')
-      break
-    default :
-      labelEl.classList.add('is-danger')
+  // [GET] - label 
+  if (labelId) {
+    const labelFragment = deepCopyFragment(template.taskLabel)
+    const res = await starryAPI.get(`/labels/${labelId}`)
+    switch (res.data.color) {
+      case 'yellow':
+        labelEl.classList.add('is-warning')
+        break
+      case 'green':
+        labelEl.classList.add('is-success')
+        break
+      case 'blue':
+        lableEl.classList.add('is-info')
+        break
+      case 'purple': 
+        labelEl.classList.add('is-primary')
+        break
+      case 'no':
+        labelEl.calssList.add('no-color')
+        break
+      default :
+        labelEl.classList.add('is-danger')
+    }
+    labelEl.textContent = res.data.body
+    render(labelFragment, labelEl)
   }
-  labelEl.textContent = res.data.body
+
+  // [DELETE] - task item 
+  btnDeleteEl.addEventListener('click', async e => {
+    const removeEl = btnDeleteEl.closest('.task-item')
+    removeEl.classList.add('task-item--delete')
+    try {
+      await starryAPI.delete(`/tasks/${id}`)
+      removeEl.remove()
+    } catch(e) {
+      projectNotification()
+      removeEl.classList.remove('task-item--delete')
+    }
+  })
+
+  checkBoxEl.addEventListener('click', async e => {
+    if(e.target.checked) {
+      await starryAPI.patch(`/tasks/${id}`, {complete: true})
+    }
+  })
 
   render(fragment, parentEl, false)
 }
 
+// Task Write Modal Component
+async function taskWriteModal(listEl, projectId) {
+  const fragment = deepCopyFragment(templates.taskWriteModal)
+  const modalEl = fragment.querySelector('.task-write-modal')
+  const formEl = modalEl.querySelector('.task-form')
+  const btnCloseEl = modalEl.querySelector('.task-write-modal__btn-close')
+  const btnSaveEl = modalEl.querySelector('.task-write-modal__btn-save')
+  const btnCancelEl = modalEl.querySelector('.task-write-modal__btn-cancel')
+
+  // [POST] - label
+  btnSaveEl.addEventListener('click', async e => {
+    // [POST] - task item
+    const payload = {
+      projectId,
+      title: formEl.elements.title.value,
+      body: formEl.elements.title.body,
+      startDate: formEl.elements.startDate.value,
+      dueDate: formEl.elements.dueDate.value,
+      complete: false
+    }
+    const res = await starryAPI.post('/tasks', payload)
+    taskItem(listEl, res.data) 
+    modalEl.remove()
+  })
+  btnCloseEl.addEventListener('click', e => {
+    modalEl.remove()
+  })
+  btnCancelEl.addEventListener('click', e => {
+    modalEl.remove()
+  })
+  
+  render(fragment, body, false)
+}
 
 const token = localStorage.getItem('token')
+
 if (token) {
   login(token)
   indexPage()
